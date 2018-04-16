d9663356461f890f6585cf4cee2771b9b7b354db
merunur <meru.nurgazy@gmail.com>
2018-04-16 15:01:44 +0300

    changing response

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req,res) =>{
-res.send(database.users);
+res.send('it is working');
 })
 
 app.post('/signin', (req,res) => {signin.handleSignin(req,res,db,bcrypt) })
@@ -34,6 +34,6 @@ app.get('/profile/:id', (req,res)=>{profile.handleProfileGet(req,res,db)})
 app.put('/image', (req,res)=>{image.handleImage(req,res,db)})
 app.post('/imageurl', (req,res)=>{image.handleApiCall(req,res)})
 
-app.listen(3000, ()=>{
-	console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, ()=>{
+	console.log(`app is running on port ${process.env.PORT}`);
 })
